0e5d69643578f70e1150967e862389c20eeee169
paulandrewc <paulandrewc90@gmail.com>
2019-02-11 22:55:42 +0000

    Made changes so that it works better out of the box.

--- src/controls/ternary-graph/ternary-graph.tsx
+++ src/controls/ternary-graph/ternary-graph.tsx
@@ -7,30 +7,22 @@ import { Component, Prop, Watch, State} from '@stencil/core';
 })
 export class TernaryGraph{
 	//Added lots of defaults to start with.
-  @Prop() recordArray: Array<{"A","B","C","X"?,"Y"?,"Z"?,"Label"}> = [
-		{"A": 5, "B": 5, "C": 90,"X": 10,"Y": 10,"Z": 80, "Label": "Super Red"},
-		{"A": 0,  "B": 0,  "C": 100,"Label": "Uber Red"},
-		{"A": 90, "B": 5, "C": 5, "Label": "Super Blue"},
-		{"A": 100,"B": 0,  "C": 0,  "Label": "Uber Blue"},
-		{"A": 5, "B": 90, "C": 5, "Label": "Super Green"},
-		{"A": 0,  "B": 100,"C": 0,  "Label": "Uber Green"},
-		{"A": 33, "B":33 ,"C": 34, "Label": "Dead Central"}
-	];
+  @Prop() recordArray: Array<{"A","B","C","X"?,"Y"?,"Z"?,"Label"}> = new Array<{"A","B","C","X"?,"Y"?,"Z"?,"Label"}>();
 	
-  @Prop({ mutable: true }) plotArray: Array<{"X","Y","X2"?,"Y2"?,"Label"}> = [];
+  @State() plotArray: Array<{"X","Y","X2"?,"Y2"?,"Label"}> = [];
 	//Corners Order Blue Green Red.
 	//Corners in the order ABC
-	@Prop() corners: {"A":{"X","Y"}, "B":{"X","Y"},"C":{"X","Y"}} = {"A":{"X":10,"Y": 80},"B": {"X":50,"Y": 10},"C": {"X":90,"Y":80}};
+	@Prop({ mutable: true }) corners: {"A":{"X","Y"}, "B":{"X","Y"},"C":{"X","Y"}} =  {"A":{"X":10,"Y": 80},"B": {"X":50,"Y": 10},"C": {"X":90,"Y":80}};
 	@Prop() circleRadius: number = 0.9;
-	@Prop() aHex: string = "#5579ff";  
-	@Prop() bHex: string = "#70c49c";
-	@Prop() cHex: string = "#ff4246";
+	@Prop() aHex: string = "#ffffff";
+	@Prop() bHex: string = "#ffffff";
+	@Prop() cHex: string = "#ffffff";
   @Prop() cFadeEndHex: string = "#ffffff";
-	@Prop() cFadeEndOpacity: string = "0.2";
+	@Prop() cFadeEndOpacity: string = "0";
 	@Prop({ mutable: true }) cFadeName:string = "cx" + this.corners.C.X +"cy"+ this.corners.C.Y + "rgb" + this.cHex.replace("#","");
 	@Prop({ mutable: true }) cFadeURL:string = "url(#" + this.cFadeName +")";
-  @Prop() abMixHex: string = "#008844";
-	@Prop() abMixOpacity: string = "0.7"
+  @Prop() abMixHex: string = "#ffffff";
+	@Prop() abMixOpacity: string = "0"
 	@Prop({ mutable: true }) abFadeName:string = "ax" + this.corners.A.X + "ay" + this.corners.A.Y +"bx"+ this.corners.B.X + "by"+ this.corners.B.Y + "argb" + this.aHex.replace("#","") + "brgb" +this.bHex.replace("#","");
 	@Prop({ mutable: true }) abFadeURL:string = "url(#" + this.abFadeName +")";
 	@Prop({ mutable: true }) abTextPathName:string = "ax" + this.corners.A.X + "ay" + this.corners.A.Y +"bx"+ this.corners.B.X + "by"+ this.corners.B.Y;
@@ -45,43 +37,74 @@ export class TernaryGraph{
 	@Prop() bcAxisLabel :string = "B to C Axis";
 	@State() isDirty: boolean;
 
-	@Watch('corners')
-  cornerPropWatcher() {
+	@Watch('aHex')
+	@Watch('bHex')
+	@Watch('cHex')
+	@Watch('cFadeEndHex')
+	@Watch('cFadeEndOpacity')
+	@Watch('abMixHex')
+	@Watch('abMixOpacity')
+  cornerAndColourPropWatcher() {
 		this.setGradientNames();
 		this.setTextPathNames();
-		this.isDirty = true;
+		this.SetDirty();
   }
 	
 	@Watch('recordArray')
 	recordArrayPropWatcher(){
 		this.updatePlotArray();
 		this.checkTotalabcPoints();
+		this.SetDirty();
+	}
+
+	@Watch('corners')
+	updateCorners()
+	{
+		this.updatePlotArray();
+		this.checkTotalabcPoints();
+		this.SetDirty();
+	}
+
+	@Watch('plotArray')
+	SetDirty(){
+		console.log("DIRTY GIRL");
+		this.isDirty = false;
 		this.isDirty = true;
 	}
 
 	componentWillLoad(){
+		if(this.recordArray.length > 0)
+		{
 		this.updatePlotArray();
+		}
 		this.setGradientNames();
 		this.setTextPathNames();
 		this.checkTotalabcPoints();
+		this.SetDirty();
 	}
 
 	setGradientNames()
-	{
-		this.cFadeName = "cx" + this.corners.C.X +"cy"+ this.corners.C.Y + "rgb" + this.cHex.replace("#","");
-		this.cFadeURL = "url(#" + this.cFadeName +")";
-		this.abFadeName =  "ax" + this.corners.A.X + "ay" + this.corners.A.Y +"bx"+ this.corners.B.X + "by"+ this.corners.B.Y + "argb" + this.aHex.replace("#","") + "brgb" +this.bHex.replace("#","");
-		this.abFadeURL = "url(#" + this.abFadeName +")";
+	{ 
+		if (this.corners !== null && this.corners !== undefined)
+		{
+			this.cFadeName = "cx" + this.corners.C.X +"cy"+ this.corners.C.Y + "rgb" + this.cHex.replace("#","");
+			this.cFadeURL = "url(#" + this.cFadeName +")";
+			this.abFadeName =  "ax" + this.corners.A.X + "ay" + this.corners.A.Y +"bx"+ this.corners.B.X + "by"+ this.corners.B.Y + "argb" + this.aHex.replace("#","") + "brgb" +this.bHex.replace("#","");
+			this.abFadeURL = "url(#" + this.abFadeName +")";
+		}
 	}
 
 	setTextPathNames()
 	{
-		this.abTextPathName = "ax" + this.corners.A.X + "ay" + this.corners.A.Y +"bx"+ this.corners.B.X + "by"+ this.corners.B.Y;
-		this.bcTextPathName = "bx" + this.corners.B.X + "by" + this.corners.B.Y +"cx"+ this.corners.C.X + "cy"+ this.corners.C.Y;
-		this.acTextPathName = "ax" + this.corners.A.X + "ay" + this.corners.A.Y +"cx"+ this.corners.C.X + "cy"+ this.corners.C.Y;
-		this.abTextPathHref = "#" + this.abTextPathName;
-		this.bcTextPathHref = "#" + this.bcTextPathName;
-		this.acTextPathHref = "#" + this.acTextPathName;
+		if (this.corners !== null && this.corners !== undefined)
+		{
+			this.abTextPathName = "ax" + this.corners.A.X + "ay" + this.corners.A.Y +"bx"+ this.corners.B.X + "by"+ this.corners.B.Y;
+			this.bcTextPathName = "bx" + this.corners.B.X + "by" + this.corners.B.Y +"cx"+ this.corners.C.X + "cy"+ this.corners.C.Y;
+			this.acTextPathName = "ax" + this.corners.A.X + "ay" + this.corners.A.Y +"cx"+ this.corners.C.X + "cy"+ this.corners.C.Y;
+			this.abTextPathHref = "#" + this.abTextPathName;
+			this.bcTextPathHref = "#" + this.bcTextPathName;
+			this.acTextPathHref = "#" + this.acTextPathName;
+		}
 	}
 
 	checkTotalabcPoints()
@@ -95,7 +118,7 @@ export class TernaryGraph{
 		};
 		if (incorrectRecords.length > 0)
 		{
-			console.log('The following records have values that do not add up to 100 \n' + incorrectRecords);
+		console.log('The following records have values that do not add up to 100 \n' + incorrectRecords);
 		 throw new Error('The following records have values that do not add up to 100 \n' + incorrectRecords);
 		}
 	}
@@ -217,18 +240,29 @@ export class TernaryGraph{
 	
   updatePlotArray() {
 		var plots = [];
-		
-		for (let i = 0; i < this.recordArray.length ; i++) {
-			if ((this.recordArray[i].A + this.recordArray[i].B + this.recordArray[i].C) == 100)
+
+		console.log("in updatePlotArray")
+		console.log(this.recordArray);
+
+		//console.log(this.recordArray.length);
+		//for (let i = 0; i < this.recordArray.length ; i++) {
+		//	console.log("GOT IN THE FOR LOOP");
+		//};
+
+		plots = this.recordArray.map( record => {
+			if ((record.A + record.B + record.C) == 100)
 			{
-		var plot = this.coord(this.recordArray[i]);
-		var plotObj = {"X":plot.X,"Y":plot.Y,"X2":plot.X2,"Y2":plot.Y2,"Label":this.recordArray[i].Label};
-		//cannot use this spread syntax because it is an array of objects which makes it not iterable.
-		//this.plotArray = [...plots,plotObj]
-		plots.push(plotObj);
+				var plot = this.coord(record);
+				var plotObj = {"X":plot.X,"Y":plot.Y,"X2":plot.X2,"Y2":plot.Y2,"Label":record.Label};
+				return plotObj;
 			}
-	};
-	this.plotArray = plots;
+		});
+		this.plotArray = plots.filter(record => record);
+		console.log("updatePlotArrayEND1")
+		console.log(plots);
+		console.log(this.plotArray);
+		console.log("updatePlotArrayEND2");
+		this.SetDirty();
 	}
 
   render() {
@@ -268,6 +302,7 @@ export class TernaryGraph{
 				<p>{record.Y}</p>
 				<p>{record.X2}</p>
 				<p>{record.Y2}</p>
+				<p>{record.Label}</p>
 			<circle class="plot" cx={record.X} cy={record.Y} r={this.circleRadius} fill="black" onClick={this.TernaryAlert.bind(this,record)} ></circle>
 			<text  class="tooltiptext" x={record.X+(this.circleRadius*1.5)} y={record.Y +this.circleRadius} fill="black" font-size="2"> {record.Label} </text>
 				<marker id="{record.Label}" markerWidth="10" markerHeight="10" refX="0" refY="1.5" orient="auto" markerUnits="strokeWidth">
